7a2a4ad528811fcd8befbd4fb7fbd749788250f5
sherifhamdym <sherif.hamdyyyy@gmail.com>
2025-04-14 20:36:06 +0200

    Add initial project structure with test framework setup

---
+++ src/test/java/test/FirstTaskTests.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class FirstTaskTests extends TestCases {
+
+    @Test
+    public void test1()
+    {
+
+    }
+}
---
+++ src/test/java/test/TestCases.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestCases {
+    protected WebDriver driver;
+    //protected Bot bot;
+
+    @BeforeMethod
+    public void setUp() {
+
+        // driver initialization
+        ChromeOptions options;
+        options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.setBrowserVersion("134");
+        options.setCapability("webSocketUrl", true);
+//        options.setImplicitWaitTimeout(Duration.ofSeconds(5));
+        driver = new ChromeDriver(options);
+
+        // bot initialization
+       // bot = new Bot(driver);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // driver termination
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
